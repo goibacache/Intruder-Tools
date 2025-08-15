d6edecc9bbf1f344b7ec56a68e1718f07b488b83
Gonzalo Olivares <39586092+goibacache@users.noreply.github.com>
2025-08-15 17:33:53 -0400

    Update IntruderToolsConfig.cs
    Unity editor settings

--- InGame/IntruderToolsConfig.cs
+++ InGame/IntruderToolsConfig.cs
@@ -1,22 +1,10 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
+#if UNITY_EDITOR
 [InitializeOnLoad]
 public class IntruderToolsConfig : MonoBehaviour
 {
     public static bool ShowColliders = false;
-
-    //public static void ZipLine_ChangeColliderVisibility(bool shouldShow)
-    //{
-    //    ShowColliders = shouldShow;
-
-    //    // Update every draw zipline object
-    //    DrawZipLineInEditor[] ziplines = GetComponents<DrawZipLineInEditor>();
-    //    foreach (DrawZipLineInEditor zip in ziplines)
-    //    {
-    //        zip.DisplayColliders = shouldShow;
-    //    }
-    //}
 }
-
-    
+#endif
